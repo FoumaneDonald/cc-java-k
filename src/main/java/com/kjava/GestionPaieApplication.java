9c4faf405646dc354db66cd1514178e0bc5da2d3
FoumaneDonald <foumaned9@gmail.com>
2026-04-21 18:08:19 +0100

    chores: project initialisation

---
+++ src/main/java/com/kjava/GestionPaieApplication.java
@@ -0,0 +1,13 @@
+package com.kjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPaieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPaieApplication.class, args);
+	}
+
+}
